2befe36414fd6af5e94d8df539aa8bbe3f68ee0c
Stephen Harrington <stephenharrington04@yahoo.com>
2018-02-03 23:06:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,14 @@
 def oxford_comma(array)
-  x = array.size
-  if x <= 0
-    nil
-  elsif x == 1
-    "#{array[0]}"
-  elsif x == 2
-    "#{array.join(" and ")}"
+  if array.count == 1
+    return "#{array.join}"
+  elsif
+    array.count == 2
+    return "#{array.join(" and ")}"
   else
-    last_item = array[x-1]
+    last = array.last
     array.pop
     string = array.join(", ")
-    string << ", and #{last_item}"
-    end
+    string << ", and #{last}"
+    return string
+  end
 end
